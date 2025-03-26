9316eaef3b79a2b17e8b13df413f09e5eaf3c796
KimeyJ <102605972+KimeyJ@users.noreply.github.com>
2025-03-26 14:40:07 -0300

    Update ejercicio_01.py

--- practico_01/ejercicio_01.py
+++ practico_01/ejercicio_01.py
@@ -1,12 +1,7 @@
-"""Bloque IF, operadores lógicos, función max y operador ternario."""
-
-
 def maximo_basico(a: float, b: float) -> float:
-    """Toma dos números y devuelve el mayor.
-
-    Restricción: No utilizar la función max"""
-    pass # Completar
-
+    if (a > b): return a
+    return b
+    pass
 
 # NO MODIFICAR - INICIO
 assert maximo_basico(10, 5) == 10
@@ -18,9 +13,7 @@ assert maximo_basico(9, 18) == 18
 
 
 def maximo_libreria(a: float, b: float) -> float:
-    """Re-escribir utilizando el built-in max.
-    Referencia: https://docs.python.org/3/library/functions.html#max
-    """
+    return max(a,b)
     pass # Completar
 
 
@@ -29,14 +22,11 @@ assert maximo_libreria(10, 5) == 10
 assert maximo_libreria(9, 18) == 18
 # NO MODIFICAR - FIN
 
-
 ###############################################################################
 
 
 def maximo_ternario(a: float, b: float) -> float:
-    """Re-escribir utilizando el operador ternario.
-    Referencia: https://docs.python.org/3/reference/expressions.html#conditional-expressions
-    """
+    return a if a>b else b
     pass # Completar
 
 
