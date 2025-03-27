f455911cedbf1c65299c427f00cbf2c8142d2556
KimeyJ <102605972+KimeyJ@users.noreply.github.com>
2025-03-27 10:34:26 -0300

    Update ejercicio_06.py

--- practico_01/ejercicio_06.py
+++ practico_01/ejercicio_06.py
@@ -3,10 +3,15 @@
 from typing import List, Union
 
 
+
+
 def numeros_al_final_basico(lista: List[Union[float, str]]) -> List[Union[float, str]]:
-    """Toma una lista de enteros y strings y devuelve una lista con todos los
-    elementos numéricos al final.
-    """
+    aux = []
+    for val in lista:
+        if type(val) == type("a"): aux.append(val)
+    for val in lista:
+        if type(val)==type(1) or type(val)==type(1.0): aux.append(val)
+    return aux
     pass # Completar
 
 
@@ -19,9 +24,10 @@ assert numeros_al_final_basico([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j", 3, 
 
 
 def numeros_al_final_comprension(lista: List[Union[float, str]]) -> List[Union[float, str]]:
-    """Re-escribir utilizando comprensión de listas."""
+    aux = [val for val in lista if type(val)==type("a")]
+    aux2 = [val for val in lista if type(val)==type(1) or type(val)==type(1.0)]
+    return aux+aux2
     pass # Completar
-
 
 # NO MODIFICAR - INICIO
 assert numeros_al_final_comprension([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j", 3, 1, 10]
@@ -29,9 +35,13 @@ assert numeros_al_final_comprension([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j"
 
 
 ###############################################################################
-
+def cmp(x):
+    return type(x)==type(1) or type(x)==type(1.0)
+    pass
 
 def numeros_al_final_sorted(lista: List[Union[float, str]]) -> List[Union[float, str]]:
+    lista.sort(key=cmp)
+    return lista
     """Re-escribir utilizando la función sorted con una custom key.
     Referencia: https://docs.python.org/3/library/functions.html#sorted
     """
@@ -50,6 +60,11 @@ def numeros_al_final_filter(lista: List[Union[float, str]]) -> List[Union[float,
     """CHALLENGE OPCIONAL - Re-escribir utilizando la función filter.
     Referencia: https://docs.python.org/3/library/functions.html#filter
     """
+    a = filter(lambda x: type(x)==type("a"),lista)
+    b = filter(lambda x: type(x)==type(1) or type(x)==type(1.0),lista)
+    auxA = list(a)
+    auxB = list(b)
+    return auxA+auxB
     pass # Completar
 
 
@@ -64,8 +79,18 @@ if __name__ == "__main__":
 
 def numeros_al_final_recursivo(lista: List[Union[float, str]]) -> List[Union[float, str]]:
     """CHALLENGE OPCIONAL - Re-escribir de forma recursiva."""
+    if not lista: return []
+    a = lista[-1]
+    b = numeros_al_final_recursivo(lista[:-1])
+    if type(a)==type("a"):
+        i = 0
+        while (i < len(b) and type(b[i])==type("a")): i += 1
+        b.insert(i,a)
+        return b
+    else: 
+        b.append(a)
+        return b
     pass # Completar
-
 
 # NO MODIFICAR - INICIO
 if __name__ == "__main__":
