d5f8a7902d367848a5eb5545edf945c633b3e08c
Luca Conti <166168538+LucaConti25@users.noreply.github.com>
2025-03-27 09:18:53 -0300

    Update ejercicio_07.py

--- practico_01/ejercicio_07.py
+++ practico_01/ejercicio_07.py
@@ -1,15 +1,11 @@
 """Slicing."""
 
+import math as mt
 
 def es_palindromo(palabra: str) -> bool:
-    """Toma un string y devuelve un booleano en base a si se lee igual al
-    derecho y al revés.
-
-    Restricción: No utilizar bucles - Usar Slices de listas.
-    Referencia: https://docs.python.org/3/tutorial/introduction.html#lists
-    """
-    pass # Completar
-
+    return True if palabra == palabra[::-1] else False
+ 
+   pass # Completar
 
 # NO MODIFICAR - INICIO
 assert not es_palindromo("amor")
@@ -20,15 +16,10 @@ assert es_palindromo("")
 
 ###############################################################################
 
-
 def mitad(palabra: str) -> str:
-    """Toma un string y devuelve la mitad. Si la longitud es impar, redondear
-    hacia arriba.
-
-    Restricción: No utilizar bucles - Usar Slices de listas.
-    Referencia: https://docs.python.org/3/tutorial/introduction.html#lists
-    """
+    return palabra[:mt.ceil(len(palabra)/2)]
     pass # Completar
+
 
 
 # NO MODIFICAR - INICIO
