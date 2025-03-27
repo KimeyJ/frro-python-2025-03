c1d02cbff6ec93570b4eb43036bba03cdb72c08e
julia <juliaamelgarejo@gmial.com>
2025-03-27 13:04:41 -0300

    ejercicio 8 terminado

--- practico_01/ejercicio_08.py
+++ practico_01/ejercicio_08.py
@@ -4,12 +4,11 @@ from typing import Any, Iterable
 
 
 def superposicion_basico(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
-    """Toma dos listas y devuelve un booleano en base a si tienen al menos 1
-    elemento en común.
-
-    Restricción: Utilizar bucles anidados.
-    """
-    pass # Completar
+    for elemento_1 in lista_1:
+        for elemento_2 in lista_2:
+            if elemento_1 == elemento_2:
+                return True      
+    return False
 
 
 # NO MODIFICAR - INICIO
@@ -23,8 +22,11 @@ assert not superposicion_basico(test_list, (2, "world", 30.85))
 
 
 def superposicion_in(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
-    """Re-Escribir utilizando un sólo bucle y el operador IN."""
-    pass # Completar
+    for elemento_2 in lista_2:
+        if elemento_2 in lista_1:
+            return True
+    return False
+
 
 
 # NO MODIFICAR - INICIO
@@ -38,10 +40,10 @@ assert not superposicion_in(test_list, (2, "world", 30.85))
 
 
 def superposicion_any(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
-    """Re-Escribir utilizando sin bucles, el operador in y la funcion any.
-    Referencia: https://docs.python.org/3/library/functions.html#any
-    """
-    pass # Completar
+    if any(elemento_2 in lista_1 for elemento_2 in lista_2):
+        return True
+    return False
+    
 
 
 # NO MODIFICAR - INICIO
@@ -55,10 +57,11 @@ assert not superposicion_any(test_list, (2, "world", 30.85))
 
 
 def superposicion_set(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
-    """Re-Escribir utilizando conjuntos (sets).
-    Referencia: https://docs.python.org/3/library/stdtypes.html#set-types-set-frozenset
-    """
-    pass # Completar
+    l1= set(lista_1)
+    l2= set(lista_2)
+    if l1.intersection(l2):
+        return True
+    return False
 
 
 # NO MODIFICAR - INICIO
