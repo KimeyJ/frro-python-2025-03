28c0df699bf6e798f30a9b4709840e8400ea015d
KimeyJ <102605972+KimeyJ@users.noreply.github.com>
2025-04-01 15:48:49 -0300

    Update ejercicio_03.py

--- practico_03/ejercicio_03.py
+++ practico_03/ejercicio_03.py
@@ -2,18 +2,15 @@
 
 
 class Persona:
-    """Clase con los siguientes miembros:
-
-    Atributos de instancia:
-    - nombre: str
-    - edad: int
-    - sexo (H hombre, M mujer): str
-    - peso: float
-    - altura: float
-
-    Métodos:
-    - es_mayor_edad(): indica si es mayor de edad, devuelve un booleano.
-    """
+    def __init__(self,nombre:str,edad:int,sexo:str,peso:float,altura:float) -> None:
+        self.nombre: str = nombre
+        self.edad: int = edad
+        self.sexo: str = sexo
+        self.peso: float = peso
+        self.altura: float = altura
+    
+    def es_mayor_edad(self) -> bool:
+        return self.edad >= 18
 
     # Completar
 
@@ -31,10 +28,14 @@ from dataclasses import dataclass
 
 @dataclass
 class Persona:
-    """Re-Escribir utilizando DataClasses"""
+    nombre: str
+    edad: int
+    sexo: str
+    peso: float
+    altura: float
 
-    # Completar
-
+    def es_mayor_edad(self) -> bool:
+        return self.edad >= 18
 
 # NO MODIFICAR - INICIO
 assert Persona("Juan", 18, "H", 85, 175.9).es_mayor_edad()
